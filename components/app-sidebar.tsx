f5b795c3b1db5a11c5a11992221a8868354c690f
vishalpalDI <vishal.p@diverseinfotech.com>
2025-07-02 02:06:36 +0530

    auth added on frontend side

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,45 +1,65 @@
 import {
   Sidebar,
-  // SidebarContent,
-  // SidebarFooter,
-  // SidebarGroup,
-  // SidebarGroupContent,
-  // SidebarGroupLabel,
-  // SidebarHeader,
-  // SidebarMenu,
-  // SidebarMenuButton,
-  // SidebarMenuItem,
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
-// import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 
-// const items = [
-//   {
-//     title: "Home",
-//     url: "#",
-//     icon: Home,
-//   },
-//   {
-//     title: "Inbox",
-//     url: "#",
-//     icon: Inbox,
-//   },
-//   {
-//     title: "Calendar",
-//     url: "#",
-//     icon: Calendar,
-//   },
-//   {
-//     title: "Search",
-//     url: "#",
-//     icon: Search,
-//   },
-//   {
-//     title: "Settings",
-//     url: "#",
-//     icon: Settings,
-//   },
-// ];
+import {
+  SignInButton,
+  SignUpButton,
+  // SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@radix-ui/react-dropdown-menu";
+import { ChevronUp } from "lucide-react";
 
 export function AppSidebar() {
-  return <Sidebar>Sidebar</Sidebar>;
+  const { user } = useUser();
+  return (
+    <Sidebar>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <UserButton /> {user?.fullName}
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="bottom"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <SignedOut>
+                  <DropdownMenuItem>
+                    <SignInButton />
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <SignUpButton>
+                      <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign Up
+                      </button>
+                    </SignUpButton>
+                  </DropdownMenuItem>
+                </SignedOut>
+                {/* <SignedIn>
+                  <UserButton />
+                </SignedIn> */}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
